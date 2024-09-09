fa3d9c3b5db229038bda41e3aa50e2c2b82d4ecb
Miggie <miguelt613377@gmail.com>
2024-09-09 15:15:58 -0400

    Weapon Base Script Started

---
+++ FeastBound/Assets/Weapons/Scripts/TestPistol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPistol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // BaseWeaponScript pistol = new BaseWeaponScript(float );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
